597d843cb2031cd6fbe0f85d336d4773a028e98f
orbartal <orbartal@gmail.com>
2018-12-02 19:51:38 -0500

    Add QueryAdapterPathsByEdgesWeight class. Will be used by GraphTextualApi to answer queries.

---
+++ code/interview.challenge.directed.weighted.graph/src/main/java/interview/challenge/directed/weighted/graph/interactor/pathsbyedgesweight/QueryAdapterPathsByEdgesWeight.java
@@ -0,0 +1,50 @@
+package interview.challenge.directed.weighted.graph.interactor.pathsbyedgesweight;
+
+import java.util.regex.Pattern;
+
+import interview.challenge.directed.weighted.graph.adapter.QueryAdapter;
+import interview.challenge.directed.weighted.graph.interactor.pathsbynodescounter.PathsByNodesCounterAlgorithm;
+import interview.challenge.directed.weighted.graph.model.Graph;
+
+public class QueryAdapterPathsByEdgesWeight implements QueryAdapter {
+
+	static private final String lineRegex = "the number of different routes from [a-z] to [a-z] with a distance of less than [0-9]+";
+	private final PathsByNodesCounterAlgorithm algorithm;
+
+	public QueryAdapterPathsByEdgesWeight() {
+		this(new PathsByNodesCounterAlgorithm());
+	}
+
+	public QueryAdapterPathsByEdgesWeight(PathsByNodesCounterAlgorithm algorithm) {
+		this.algorithm = algorithm;
+	}
+
+	@Override
+	public boolean isMatch(String line) {
+		return Pattern.matches(lineRegex, line);
+	}
+
+	@Override
+	public String execute(Graph graph, String line) {
+		String[] args = getParams(line);
+		int result = execute(graph, args);
+		return result + "";
+	}
+
+	private int execute(Graph graph, String[] args) {
+		String start = args[0];
+		String end = args[1];
+		int min = Integer.parseInt(args[2]);
+		int max = Integer.parseInt(args[3]);
+		return algorithm.getNumberOfPaths(graph, start, end, min, max);
+	}
+
+	private String[] getParams(String line) {
+		String[] parts = line.split(" ");
+		String number = parts[parts.length - 1];
+		String min = "1", max = number;
+		String[] result = { parts[6], parts[8], min, max };
+		return result;
+	}
+
+}
---
+++ code/interview.challenge.directed.weighted.graph/src/test/java/interview/challenge/directed/weighted/graph/interactor/pathsbyedgesweight/QueryAdapterPathsByEdgesWeightTest.java
@@ -0,0 +1,52 @@
+package interview.challenge.directed.weighted.graph.interactor.pathsbyedgesweight;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import interview.challenge.directed.weighted.graph.interactor.pathsbynodescounter.PathsByNodesCounterAlgorithm;
+import interview.challenge.directed.weighted.graph.model.Graph;
+
+@ExtendWith(MockitoExtension.class)
+public class QueryAdapterPathsByEdgesWeightTest {
+
+	@Mock
+	private Graph graph;
+
+	@Mock
+	private PathsByNodesCounterAlgorithm algorithm;
+
+	@InjectMocks
+	private QueryAdapterPathsByEdgesWeight fixture;
+
+	@Test
+	void testIsMatchValidInputMaximum() {
+		String input = "The number of different routes from C to C with a distance of less than 30".toLowerCase();
+		boolean actual = fixture.isMatch(input);
+		Assertions.assertTrue(actual);
+	}
+
+	@Test
+	void testIsMatchInValidInput() {
+		String input = "The distance of the route A-E-B-C-D".toLowerCase();
+		boolean actual = fixture.isMatch(input);
+		Assertions.assertFalse(actual);
+	}
+
+	@Test
+	void testExecuteValidMaximum() {
+		// setup
+		String line = "The number of different routes from C to C with a distance of less than 30".toLowerCase();
+		Integer expected = 7;
+		Mockito.when(algorithm.getNumberOfPaths(graph, "c", "c", 1, 30)).thenReturn(expected);
+		// execute
+		String actual = fixture.execute(graph, line);
+		// verify
+		Assertions.assertEquals(expected + "", actual);
+	}
+
+}
